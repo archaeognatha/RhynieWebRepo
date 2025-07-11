3ee8c3eb666a43d5a0a16c7daa02736ddbc44ad1
archaeognatha <tannermfrank@gmail.com>
2025-07-11 14:35:47 -0700

    Added R file to summarize metrics

--- MetricsComparisons/PlotMetrics.R
+++ MetricsComparisons/PlotMetrics.R
@@ -4,12 +4,16 @@ library(tidyverse)
 # Read and inspect data
 df <- read_csv("TrophospeciesMetrics.csv")
 
-# Check column names and unique analyses
-glimpse(df)
-analyses <- unique(df$Analysis)
+# subset the dataframe with the first 100 rhynie replicates and remove the lumped terr/aqu webs 
+# ...to make the plots below easier to read
+df_small <- df %>%
+  filter(!Analysis %in% c("Rhynie_terr_lumped", "Rhynie_aqu_lumped")) %>%
+  group_by(Analysis) %>%
+  slice_head(n = 100) %>%
+  ungroup()
 
 # Plot C vs S, colored by Analysis
-ggplot(df, aes(x = S, y = C, color = Analysis)) +
+ggplot(df_small, aes(x = S, y = C, color = Analysis)) +
   geom_point(size = 2) +
   theme_classic() +
   labs(title = "Connectance vs. Species richness",
@@ -17,7 +21,7 @@ ggplot(df, aes(x = S, y = C, color = Analysis)) +
        y = "Connectance (C)")
 
 # Plot L/S vs S, colored by Analysis
-ggplot(df, aes(x = S, y = L_D, color = Analysis)) +
+ggplot(df_small, aes(x = S, y = L_D, color = Analysis)) +
   geom_point(size = 2) +
   theme_classic() +
   labs(title = "Link Density vs. Species richness",
@@ -25,7 +29,7 @@ ggplot(df, aes(x = S, y = L_D, color = Analysis)) +
        y = "Link Density (L/S)")
 
 # mean trophic level (mean_NTP) vs S
-ggplot(df, aes(x = S, y = Mean_NTP, color = Analysis)) +
+ggplot(df_small, aes(x = S, y = Mean_NTP, color = Analysis)) +
   geom_point(size = 2) +
   theme_classic() +
   labs(title = "Mean Trophic Position vs Species Richness",
@@ -34,7 +38,7 @@ ggplot(df, aes(x = S, y = Mean_NTP, color = Analysis)) +
 
 
 # max trophic level (mean_NTP) vs S
-ggplot(df, aes(x = S, y = Max_NTP, color = Analysis)) +
+ggplot(df_small, aes(x = S, y = Max_NTP, color = Analysis)) +
   geom_point(size = 2) +
   theme_classic() +
   labs(title = "Max Trophic Position vs Species Richness",
@@ -42,7 +46,7 @@ ggplot(df, aes(x = S, y = Max_NTP, color = Analysis)) +
        y = "Max Trophic Position")
 
 # generality (mean_InDegree) vs S
-ggplot(df, aes(x = S, y = Mean_NTP, color = Analysis)) +
+ggplot(df_small, aes(x = S, y = Mean_NTP, color = Analysis)) +
   geom_point(size = 2) +
   theme_classic() +
   labs(title = "Mean In Degree (generality) vs Species Richness",
@@ -50,7 +54,7 @@ ggplot(df, aes(x = S, y = Mean_NTP, color = Analysis)) +
        y = "Mean In Degree")
 
 # incoherence (q) vs S
-ggplot(df, aes(x = S, y = q_inCoherence, color = Analysis)) +
+ggplot(df_small, aes(x = S, y = q_inCoherence, color = Analysis)) +
   geom_point(size = 2) +
   theme_classic() +
   labs(title = "q (incoherence) vs Species Richness",
@@ -58,7 +62,7 @@ ggplot(df, aes(x = S, y = q_inCoherence, color = Analysis)) +
        y = "q")
 
 # max chain length vs S
-ggplot(df, aes(x = S, y = Mean_longest_chain, color = Analysis)) +
+ggplot(df_small, aes(x = S, y = Mean_longest_chain, color = Analysis)) +
   geom_point(size = 2) +
   theme_classic() +
   labs(title = "Longest Chain Length vs Species Richness",
@@ -66,7 +70,7 @@ ggplot(df, aes(x = S, y = Mean_longest_chain, color = Analysis)) +
        y = "Mean Longest Chain Length")
 
 # mean path length vs S
-ggplot(df, aes(x = S, y = Mean_path_len, color = Analysis)) +
+ggplot(df_small, aes(x = S, y = Mean_path_len, color = Analysis)) +
   geom_point(size = 2) +
   theme_classic() +
   labs(title = "Mean path Length vs Species Richness",
@@ -74,12 +78,20 @@ ggplot(df, aes(x = S, y = Mean_path_len, color = Analysis)) +
        y = "Mean path length between nodes")
 
 # % apex taxa vs S
-ggplot(df, aes(x = S, y = Top, color = Analysis)) +
+ggplot(df_small, aes(x = S, y = Top, color = Analysis)) +
   geom_point(size = 2) +
   theme_classic() +
   labs(title = "Trophic Top Species vs Species Richness",
        x = "Number of species (S)",
        y = "Percentage of nodes without predators")
+
+# % primary consumer taxa vs S
+ggplot(df_small, aes(x = S, y = Top, color = Analysis)) +
+  geom_point(size = 2) +
+  theme_classic() +
+  labs(title = "Primary consumers vs Species Richness",
+       x = "Number of species (S)",
+       y = "Percentage of primary consumers")
 
 # Group comparisons
 
@@ -137,6 +149,12 @@ ggplot(df, aes(x = Analysis, y = MeanInDegree , fill = Analysis)) +
   theme_classic() +
   labs(title = "Mean in-degree in Rhynie and Modern webs")
 
+# boxplot of variation of trophic generality (std. dev. in-degree)
+ggplot(df, aes(x = Analysis, y = StdInDegree , fill = Analysis)) +
+  geom_boxplot() +
+  theme_classic() +
+  labs(title = "Standard deviation of in-degree in Rhynie and Modern webs")
+
 # boxplot of % carnivores
 ggplot(df, aes(x = Analysis, y = Carniv, fill = Analysis)) +
   geom_boxplot() +
@@ -155,9 +173,29 @@ ggplot(df, aes(x = Analysis, y = Top, fill = Analysis)) +
   theme_classic() +
   labs(title = "% taxa in Rhynie and Modern webs without predators")
 
-# t-test on connectance
-t.test(C ~ lumped, data = rhynie_df)
+### Statistical comparisons of samples
 
-# Example: t-test on mean trophic level
-t.test(mean_NTP ~ group, data = modern_vs_rhynie)
+# label webs as Devonian or Modern
+df <- df %>%
+  mutate(Period = ifelse(startsWith(Analysis, "Rhynie"),"devonian","modern")) %>%
+  mutate(Period = as.factor(Period))
 
+# subset dataframe for comparing full (Trophic Species) rhynie to modern webs
+df_unlumped <- df %>%
+  filter(!startsWith(Analysis, "Rhynie")|(startsWith(Analysis, "Rhynie_all")&!endsWith(Analysis,"lumped"))) %>%
+  mutate(Analysis = as.factor(Analysis)) 
+
+# subset dataframe for comparing lumped rhynie to modern webs
+df_lumped <- df %>%
+  filter(!startsWith(Analysis, "Rhynie")|Analysis == "Rhynie_all_lumped") %>%
+  mutate(Analysis = as.factor(Analysis)) 
+
+# subset dataframe for comparing rhynie web to terrestrial and aquatic subsets (not lumped)
+df_habitats <- df %>%
+  filter(startsWith(Analysis, "Rhynie")&!endsWith(Analysis,"lumped")) %>%
+  mutate(Analysis = as.factor(Analysis)) 
+
+# subset dataframe for comparing full and lumped rhynie webs (complete)
+df_rhynielumping <- df %>%
+  filter(startsWith(Analysis, "Rhynie_all")) %>%
+  mutate(Analysis = as.factor(Analysis)) 
